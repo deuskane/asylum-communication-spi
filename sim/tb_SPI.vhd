c86255febc4d6857378c34ac7ff39b24bc180035
deuskane <deuskane@gmail.com>
2025-05-30 09:58:14 +0200

    Testbench factoring

--- sim/tb_SPI.vhd
+++ sim/tb_SPI.vhd
@@ -110,7 +110,7 @@ begin
       ,sclk_o         => sclk_o     
       ,cs_b_o         => cs_b_o     
       ,mosi_o         => mosi_o     
-      ,miso_i         => miso_i     
+      ,miso_i         => mosi_o     
        );
 
   ------------------------------------------------
@@ -127,182 +127,90 @@ begin
     -- outputs: All dut design with clk
     
   tb_gen: process is
+
+    variable x : std_logic_vector(1 downto 0) := "10";
+    
+    -------------------------------------------------------
+    -- cfg
+    -------------------------------------------------------
+    procedure cfg
+      (constant cpol   : in std_logic;
+       constant cpha   : in std_logic
+       ) is
+
+    begin
+      
+      
+      report "[TESTBENCH] Configure CPOL "& std_logic'image(cpol) & " - CPHA "& std_logic'image(cpol);
+      arst_b_i    <= '0';
+      cpol_i      <= cpol;
+      cpha_i      <= cpha;
+      run(1);
+      arst_b_i    <= '1';
+      run(1);
+    end cfg;
+
+    
+    -------------------------------------------------------
+    -- tx_1byte
+    -------------------------------------------------------
+    procedure tx_1byte
+      (constant byte0   : in std_logic_vector(8-1 downto 0)
+       ) is
+      
+    begin
+      report "[TESTBENCH] TX 1 byte";
+
+      tx_tvalid_i <= '1';
+      tx_tdata_i  <= byte0;
+      wait until tx_tready_o = '0';
+      tx_tvalid_i <= '0';
+      tx_tdata_i  <= X"00";
+      wait until tx_tready_o = '1';
+    end tx_1byte;
+
+    procedure tx_2byte
+      (constant byte0   : in std_logic_vector(8-1 downto 0);
+       constant byte1   : in std_logic_vector(8-1 downto 0)
+       ) is
+      
+    begin
+      report "[TESTBENCH] TX 2 bytes in burst";
+
+      tx_tvalid_i <= '1';
+      tx_tdata_i  <= byte0;
+      wait until tx_tready_o = '0';
+      tx_tvalid_i <= '1';
+      tx_tdata_i  <= byte1;
+      wait until tx_tready_o = '1';
+      wait until tx_tready_o = '0';
+      tx_tvalid_i <= '0';
+      tx_tdata_i  <= X"00";
+      wait until tx_tready_o = '1';
+    end tx_2byte;
+
+
   begin  -- process tb_gen
     report "[TESTBENCH] Test Begin";
 
     -- Réinitialisation
     run(10);
-    arst_b_i    <= '0';
-
     prescaler_i <= X"04";
-    cpol_i      <= '0';
-    cpha_i      <= '0';
     rx_tready_i <= '1';
-    miso_i      <= '0';
-    run(10);
-    arst_b_i <= '1';
 
+    for cpol in 0 to 1 loop
+      for cpha in 0 to 1 loop
+
+        run(100);
+        cfg(x(cpol),x(cpha));
+        run(100);
+        tx_1byte(X"55");
+        run(100);
+        tx_2byte(X"0F",X"F0");
+        
+      end loop;  -- cpha
+    end loop;  -- cpol
     
-    report "[TESTBENCH] (CPOL 0 - CPHA 0) Transmission d'un seul byte";
-    run(100);
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"55";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-
-    report "[TESTBENCH] (CPOL 0 - CPHA 0) Transmission de 2 bytes";
-    run(100);
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"F0";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"0F";
-    wait until tx_tready_o = '1';
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    report "[TESTBENCH] (CPOL 0 - CPHA 1) Transmission d'1 byte ";
-    run(100);
-    arst_b_i    <= '0';
-    cpol_i      <= '0';
-    cpha_i      <= '1';
-    run(1);
-    arst_b_i    <= '1';
-    run(1);
-
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"3C";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    report "[TESTBENCH] (CPOL 0 - CPHA 1) Transmission de 2 bytes";
-    run(100);
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"F0";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"0F";
-    wait until tx_tready_o = '1';
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    report "[TESTBENCH] (CPOL 1 - CPHA 0) Transmission d'1 byte ";
-    run(100);
-    arst_b_i    <= '0';
-    cpol_i      <= '1';
-    cpha_i      <= '0';
-    run(1);
-    arst_b_i    <= '1';
-    run(1);
-
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"3C";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    report "[TESTBENCH] (CPOL 1 - CPHA 0) Transmission de 2 bytes";
-    run(100);
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"F0";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"0F";
-    wait until tx_tready_o = '1';
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    report "[TESTBENCH] (CPOL 1 - CPHA 1) Transmission d'1 byte ";
-    run(100);
-    arst_b_i    <= '0';
-    cpol_i      <= '1';
-    cpha_i      <= '1';
-    run(1);
-    arst_b_i    <= '1';
-    run(1);
-
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"3C";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    report "[TESTBENCH] (CPOL 1 - CPHA 1) Transmission de 2 bytes";
-    run(100);
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"F0";
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '1';
-    tx_tdata_i  <= X"0F";
-    wait until tx_tready_o = '1';
-    wait until tx_tready_o = '0';
-    tx_tvalid_i <= '0';
-    tx_tdata_i  <= X"00";
-    wait until tx_tready_o = '1';
-
-    
---    -- **Test 3 : SPI avec CPOL=1, CPHA=1**
---    run(10);
---    cpol <= '1';
---    cpha <= '1';
---    tx_tvalid <= '1';
---    tx_tdata <= "11110000";
---    wait until tx_tready = '0';
---    tx_tvalid <= '0';
---    tx_tdata <= "00000000";
---    wait until tx_tready = '1';
---    assert rx_tvalid = '1' and rx_tdata = "11110000"
---      report "Test 3 réussi - Configuration CPOL=1, CPHA=1 validée."
---      severity note;
---    
---    -- **Test 4 : Désactivation de SS après transmission**
---    run(10);
---    disable_ss <= '1';
---    tx_tvalid <= '1';
---    tx_tdata <= "00001111";
---    wait until tx_tready = '0';
---    tx_tvalid <= '0';
---    tx_tdata <= "00000000";
---    wait until tx_tready = '1';
---    assert ss = '1'
---      report "Test 4 réussi - SS désactivé après transaction."
---      severity note;
---    
---    -- **Test 5 : Désactivation de SCLK après transmission**
---    run(10);
---    disable_sclk <= '1';
---    tx_tvalid <= '1';
---    tx_tdata <= "00000000";
---    wait until tx_tready = '0';
---    wait until tx_tready = '1';
---    assert sclk = cpol
---      report "Test 5 réussi - SCLK désactivé après transaction."
---      severity note;
---    
---    -- **Test 6 : Vérification d'un cas d'erreur (pas de donnée TX)**
---    run(10);
---    tx_tvalid <= '0';
---    run(100);
---    assert rx_tvalid = '0'
---      report "Test 6 réussi - Pas de donnée reçue sans TX valid."
---      severity note;
---    
-    -- **Fin du test**
-    report "Tous les tests SPI terminés !";
-
     test_ok   <= '1';
     run(100);
     test_done <= '1';

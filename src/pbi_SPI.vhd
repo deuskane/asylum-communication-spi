38eac9773fdc0d89c167b61856539fdd9ae37be3
deuskane <deuskane@gmail.com>
2025-06-13 09:14:43 +0200

    Add pbi_spi

--- src/pbi_SPI.vhd
+++ src/pbi_SPI.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosiere
 -- Company    : 
 -- Created    : 2017-03-30
--- Last update: 2025-05-14
+-- Last update: 2025-06-13
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -16,10 +16,7 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author  Description
--- 2025-05-14  0.3      mrosiere Delete parameters DATA_OE_FORCE,
---                               csr use DATA_OE_INIT
--- 2025-03-05  0.2      mrosiere use csr from regtool
--- 2017-03-30  0.1      mrosiere Created
+-- 2025-06-13  0.1      mrosiere Created
 -------------------------------------------------------------------------------
 
 library IEEE;
@@ -28,45 +25,44 @@ use     IEEE.numeric_std.ALL;
 library work;
 library work;
 use     work.pbi_pkg.all;
-use     work.GPIO_csr_pkg.all;
+use     work.SPI_csr_pkg.all;
 
-entity pbi_GPIO is
+entity pbi_SPI is
   generic(
-    NB_IO            : natural:=8;     -- Number of IO. Must be <= SIZE_DATA
-    DATA_OE_INIT     : std_logic_vector; -- Direction of the IO after a reset
-    IT_ENABLE        : boolean:=false    -- GPIO can generate interruption
+    USER_DEFINE_PRESCALER : boolean; -- Parameters to use the enable the User define Prescaler
+    PRESCALER_RATIO       : std_logic_vector(8-1 downto 0) -- Default value for prescaler ratio
     );
   port   (
     clk_i            : in    std_logic;
-    cke_i            : in    std_logic;
-    arstn_i          : in    std_logic; -- asynchronous reset
+    arst_b_i         : in    std_logic; -- asynchronous reset
 
     -- Bus
     pbi_ini_i        : in    pbi_ini_t;
     pbi_tgt_o        : out   pbi_tgt_t;
     
-    -- To/From IO
-    data_i           : in    std_logic_vector (NB_IO-1     downto 0);
-    data_o           : out   std_logic_vector (NB_IO-1     downto 0);
-    data_oe_o        : out   std_logic_vector (NB_IO-1     downto 0);
-
-    -- To/From IT Ctrl
-    interrupt_o      : out   std_logic;
-    interrupt_ack_i  : in    std_logic
+    -- SPI Interface
+    sclk_o           : out std_logic;
+    sclk_oe_o        : out std_logic;
+    cs_b_o           : out std_logic;
+    cs_b_oe_o        : out std_logic;
+    mosi_o           : out std_logic;
+    mosi_oe_o        : out std_logic;
+    miso_i           : in  std_logic
     );
 
-end entity pbi_GPIO;
+end entity pbi_SPI;
 
-architecture rtl of pbi_GPIO is
+architecture rtl of pbi_SPI is
 
-  signal sw2hw                  : GPIO_sw2hw_t;
-  signal hw2sw                  : GPIO_hw2sw_t;
+  signal sw2hw                  : SPI_sw2hw_t;
+  signal hw2sw                  : SPI_hw2sw_t;
 
 begin  -- architecture rtl
 
-  ins_csr : entity work.GPIO_registers(rtl)
+  ins_csr : entity work.SPI_registers(rtl)
   generic map(
-    DATA_OE_INIT     => DATA_OE_INIT 
+    USER_DEFINE_PRESCALER => USER_DEFINE_PRESCALER,
+    PRESCALER_RATIO       => PRESCALER_RATIO      
     )
   port map(
     clk_i     => clk_i           ,
@@ -77,22 +73,35 @@ begin  -- architecture rtl
     hw2sw_i   => hw2sw   
   );
 
-  ins_GPIO : entity work.GPIO(rtl)
-  generic map(
-    NB_IO            => NB_IO          ,
-    IT_ENABLE        => IT_ENABLE    
-    )
-  port map(
-    clk_i            => clk_i          ,
-    cke_i            => cke_i          ,
-    arstn_i          => arstn_i        ,
-    data_i           => data_i         ,
-    data_o           => data_o         ,
-    data_oe_o        => data_oe_o      ,
-    interrupt_o      => interrupt_o    ,
-    interrupt_ack_i  => interrupt_ack_i,
-    sw2hw_i          => sw2hw          ,
-    hw2sw_o          => hw2sw 
+  ins_spi_master : entity work.spi_master(rtl)
+    generic map(
+      PRESCALER_WIDTH      => 8
+      )
+    port map
+    ( clk_i                 => clk_i
+     ,arst_b_i              => sw2hw.cfg.spi_enable
+     ,tx_tvalid_i           => sw2hw.data.valid
+     ,tx_tready_o           => hw2sw.data.ready
+     ,tx_tdata_i            => sw2hw.data.value
+     ,rx_tvalid_o           => hw2sw.data.valid
+     ,rx_tready_i           => sw2hw.data.ready
+     ,rx_tdata_o            => hw2sw.data.value
+     ,cmd_tvalid_i          => sw2hw.cmd.valid
+     ,cmd_tready_o          => hw2sw.cmd.ready
+     ,cmd_last_transfer_i   => sw2hw.cmd.last
+     ,cmd_enable_rx_i       => sw2hw.cmd.enable_rx
+     ,cmd_enable_tx_i       => sw2hw.cmd.enable_tx
+     ,cmd_nb_bytes_i        => sw2hw.cmd.nb_bytes
+     ,cfg_cpol_i            => sw2hw.cfg.cpol
+     ,cfg_cpha_i            => sw2hw.cfg.cpha
+     ,cfg_prescaler_ratio_i => sw2hw.prescaler.ratio
+     ,sclk_o                => sclk_o   
+     ,sclk_oe_o             => sclk_oe_o
+     ,cs_b_o                => cs_b_o   
+     ,cs_b_oe_o             => cs_b_oe_o
+     ,mosi_o                => mosi_o   
+     ,mosi_oe_o             => mosi_oe_o
+     ,miso_i                => miso_i   
     );
   
 end architecture rtl;

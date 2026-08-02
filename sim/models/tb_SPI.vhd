2d707a08783a12ac348db3494d0eb5c23e1f1a6b
deuskane <deuskane@gmail.com>
2026-08-02 09:43:51 +0200

    Testbench : use UVVM sbi, dut is sbi_spi instead spi_master

--- sim/models/tb_SPI.vhd
+++ sim/models/tb_SPI.vhd
@@ -4,219 +4,200 @@
 -------------------------------------------------------------------------------
 -- File       : tb_SPI.vhd
 -- Author     : mrosiere
--- Company    : 
+-- Company    :
 -- Created    : 2025-05-31
--- Last update: 2026-01-17
--- Platform   : 
+-- Last update: 2026-08-01
+-- Platform   :
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
--- Description: 
+-- Description: UVVM/SBI testbench for the SBI SPI DUT
 -------------------------------------------------------------------------------
 -- Copyright (c) 2025
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author   Description
 -- 2025-05-31  1.0      mrosiere Created
+-- 2026-08-01  1.1      mrosiere Use UVVM SBI VIP and clock generator
 -------------------------------------------------------------------------------
 
-library IEEE;
-use     IEEE.STD_LOGIC_1164.ALL;
-use     IEEE.numeric_std.ALL;
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+
+library uvvm_util;
+context uvvm_util.uvvm_util_context;
+
+library bitvis_vip_sbi;
+use     bitvis_vip_sbi.sbi_bfm_pkg.all;
+
 library asylum;
+use     asylum.sbi_pkg.all;
 use     asylum.spi_pkg.all;
-library work;
-use     work.tb_SPI_pkg.all;
- 
+use     asylum.SPI_csr_pkg.all;
+
 entity tb is
 end tb;
- 
+
 architecture sim of tb is
-  -----------------------------------------------------
-  -- Test signals
-  -----------------------------------------------------
-  signal test_en                 : boolean   := false;
-  signal test_done               : std_logic := '0';
-  signal test_ok                 : std_logic := '0';
-                                 
-  constant PRESCALER_WIDTH       : integer := 8;
-                                 
-                                 
-  -- Déclaration des signaux     
-  signal  clk_i                  : std_logic;
-  signal  dut_ifi                : spi_master_ifi_t(cfg_prescaler_ratio_i(PRESCALER_WIDTH-1 downto 0),
-                                                   cmd_nb_bytes_i       (3 downto 0));
-  signal  dut_ifo                : spi_master_ifo_t;
 
-  signal  miso_i                 : std_logic;
-  signal  RSTNeg                 : std_logic;
-  signal  WPNeg                  : std_logic;
-  signal  HOLDNeg                : std_logic;
+  constant C_SCOPE         : string := "TB_SPI";
+  constant SPI_ADDR_WIDTH  : natural := 2;
+  constant SPI_DATA_WIDTH  : natural := 8;
 
-  procedure run
-    (constant n     : in positive;          -- nb cycle
-     constant pol   : in string := "pos"
-     ) is
-    
-  begin
-    run(n,pol,clk_i);
-  end run;
-  
+  constant C_SBI_BFM_CONFIG_SPI : t_sbi_bfm_config := (
+    max_wait_cycles            => 1000,
+    max_wait_cycles_severity   => failure,
+    use_fixed_wait_cycles_read => false,
+    fixed_wait_cycles_read     => 0,
+    clock_period               => -1 ns,
+    clock_period_margin        => 0 ns,
+    clock_margin_severity      => TB_ERROR,
+    setup_time                 => -1 ns,
+    hold_time                  => -1 ns,
+    bfm_sync                   => SYNC_ON_CLOCK_ONLY,
+    match_strictness           => MATCH_EXACT,
+    id_for_bfm                 => ID_BFM,
+    id_for_bfm_wait            => ID_BFM_WAIT,
+    id_for_bfm_poll            => ID_BFM_POLL,
+    use_ready_signal           => true
+  );
+
+  signal clk_i             : std_logic := '0';
+  signal clk_ena           : boolean   := true;
+  signal arst_b_i          : std_logic := '0';
+
+  signal sbi_ini           : sbi_ini_t(addr (SPI_ADDR_WIDTH-1 downto 0),
+                                       wdata(SPI_DATA_WIDTH-1 downto 0));
+  signal sbi_tgt           : sbi_tgt_t(rdata(SPI_DATA_WIDTH-1 downto 0));
+  signal sbi_if            : t_sbi_if(addr (SPI_ADDR_WIDTH-1 downto 0),
+                                      wdata(SPI_DATA_WIDTH-1 downto 0),
+                                      rdata(SPI_DATA_WIDTH-1 downto 0));
+
+  signal sclk_o            : std_logic;
+  signal cs_b_o            : std_logic;
+  signal mosi_o            : std_logic;
+  signal miso_i            : std_logic;
+  signal RSTNeg            : std_logic;
+  signal WPNeg             : std_logic;
+  signal HOLDNeg           : std_logic;
+
 begin
-  ------------------------------------------------
-  -- DUT
-  ------------------------------------------------
-  -- Instanciation du contrôleur SPI_master
-  dut: spi_master
-    port map
-      (clk_i                => clk_i
-      ,arst_b_i             => dut_ifi.arst_b_i
-      ,tx_tdata_i           => dut_ifi.tx_tdata_i 
-      ,tx_tvalid_i          => dut_ifi.tx_tvalid_i
-      ,tx_tready_o          => dut_ifo.tx_tready_o
-      ,rx_tdata_o           => dut_ifo.rx_tdata_o 
-      ,rx_tvalid_o          => dut_ifo.rx_tvalid_o
-      ,rx_tready_i          => dut_ifi.rx_tready_i
-      ,cfg_cpol_i           => dut_ifi.cfg_cpol_i     
-      ,cfg_cpha_i           => dut_ifi.cfg_cpha_i     
-      ,cfg_prescaler_ratio_i=> dut_ifi.cfg_prescaler_ratio_i
-      ,cfg_loopback_i       => dut_ifi.cfg_loopback_i
-      ,cmd_tvalid_i         => dut_ifi.cmd_tvalid_i
-      ,cmd_tready_o         => dut_ifo.cmd_tready_o
-      ,cmd_tlast_i          => dut_ifi.cmd_tlast_i
-      ,cmd_enable_tx_i      => dut_ifi.cmd_enable_tx_i
-      ,cmd_enable_rx_i      => dut_ifi.cmd_enable_rx_i
-      ,cmd_nb_bytes_i       => dut_ifi.cmd_nb_bytes_i
-      ,sclk_o               => dut_ifo.sclk_o     
-      ,cs_b_o               => dut_ifo.cs_b_o     
-      ,mosi_o               => dut_ifo.mosi_o     
-      ,miso_i               => miso_i
-      );
 
-  ------------------------------------------------
-  -- Memory Model
-  ------------------------------------------------
-  RSTNeg  <= '1';
-  WPNeg   <= '1';
-  HOLDNeg <= '1';
+  arst_b_i <= '0', '1' after 100 ns;
+  RSTNeg   <= '1';
+  WPNeg    <= '1';
+  HOLDNeg  <= '1';
 
---  mem : entity work.s25fl512s(vhdl_behavioral_static_memory_allocation)
---    generic map
---    (TimingModel       => "S25FL512SAGMFI010_F_30pF"
---    ,mem_file_name     => "memory.mem"
---    ,otp_file_name     => "memoryOTP.mem"
---    ,UserPreload       => True
---    ,TimingChecksOn    => True
---    ,MsgOn             => True
---    ,XOn               => True
---     )
---    port map
---    (SI            => dut_ifo.mosi_o -- serial data input  /IO0
---    ,SO            => miso_i         -- serial data output /IO1
---    ,SCK           => dut_ifo.sclk_o -- serial clock input     
---    ,CSNeg         => dut_ifo.cs_b_o -- chip select input      
---    ,RSTNeg        => RSTNeg
---    ,WPNeg         => WPNeg          -- write protect input/IO2
---    ,HOLDNeg       => HOLDNeg        -- hold input         /IO3
---     );
---
+  clock_generator(clk_i, clk_ena, 20 ns, "TB Clock");
+
+  dut : sbi_SPI
+    generic map (
+      NAME                  => "SPI"
+     ,USER_DEFINE_PRESCALER => false
+     ,PRESCALER_RATIO       => x"02"
+     ,DEPTH_CMD             => 4
+     ,DEPTH_TX              => 4
+     ,DEPTH_RX              => 4
+    )
+    port map (
+      clk_i      => clk_i
+     ,arst_b_i   => arst_b_i
+     ,sbi_ini_i  => sbi_ini
+     ,sbi_tgt_o  => sbi_tgt
+     ,sclk_o     => sclk_o
+     ,sclk_oe_o  => open
+     ,cs_b_o     => cs_b_o
+     ,cs_b_oe_o  => open
+     ,mosi_o     => mosi_o
+     ,mosi_oe_o  => open
+     ,miso_i     => miso_i
+    );
 
   mem : entity work.m25p40(vhdl_behavioral)
-      generic map
-      (mem_file_name     => "memory.mem"
-      ,UserPreload       => True
-      ,DebugInfo         => True
-      ,TimingChecksOn    => True
-      ,MsgOn             => True
-      ,XOn               => True
-       )
-      port map
-      (D             => dut_ifo.mosi_o -- serial data input  /IO0
-      ,Q             => miso_i         -- serial data output /IO1
-      ,C             => dut_ifo.sclk_o -- serial clock input
-      ,SNeg          => dut_ifo.cs_b_o -- chip select input
-      ,WNeg          => WPNeg          -- write protect input/IO2
-      ,HOLDNeg       => HOLDNeg        -- hold input         /IO3
-       );
-    
-  
-  ------------------------------------------------
-  -- Clock process
-  ------------------------------------------------
-  gen_clk: process is
-    begin  -- process gen_clk
-      clk_i <= '0';
+    generic map (
+      mem_file_name  => "memory.mem"
+     ,UserPreload    => true
+     ,DebugInfo      => true
+     ,TimingChecksOn => true
+     ,MsgOn          => true
+     ,XOn            => true
+    )
+    port map (
+      D       => mosi_o
+     ,Q       => miso_i
+     ,C       => sclk_o
+     ,SNeg    => cs_b_o
+     ,WNeg    => WPNeg
+     ,HOLDNeg => HOLDNeg
+    );
 
-      while test_done = '0'
-      loop
-        wait for 5 ns;
+  sbi_ini.cs                               <= sbi_if.cs;
+  sbi_ini.addr                             <= std_logic_vector(sbi_if.addr(SPI_ADDR_WIDTH-1 downto 0));
+  sbi_ini.re                               <= sbi_if.rena;
+  sbi_ini.we                               <= sbi_if.wena;
+  sbi_ini.wdata                            <= sbi_if.wdata(SPI_DATA_WIDTH-1 downto 0);
+  sbi_if.ready                             <= sbi_tgt.ready;
+  sbi_if.rdata(SPI_DATA_WIDTH-1 downto 0) <= sbi_tgt.rdata;
 
-        clk_i <= not clk_i;
-      end loop;
+  process
+  begin
+    sbi_if <= init_sbi_if_signals(SPI_ADDR_WIDTH, SPI_DATA_WIDTH);
+    wait until arst_b_i = '1';
+    wait until rising_edge(clk_i);
 
-      wait; -- end process
-    end process gen_clk;  
-  
-    ------------------------------------------------
-    -- Test process
-    ------------------------------------------------
-    -- purpose: Testbench process
-    -- type   : combinational
-    -- inputs : 
-    -- outputs: All dut design with clk
-    
-    tb_gen: process is
-    
-    begin  -- process tb_gen
-      report "[TESTBENCH] Test begin";
-      dut_ifi.arst_b_i              <= '1';
+    log(ID_SEQUENCER, "Reset released, starting SPI SBI test", C_SCOPE);
 
-      -- Réinitialisation
-      run(10);
-      dut_ifi.tx_tvalid_i           <= '0';
-      dut_ifi.cmd_tvalid_i          <= '0';
-      dut_ifi.rx_tready_i           <= '0';
+    log(ID_LOG_HDR, "Configuration of the Prescaler (Divide by 32)", C_SCOPE);
+    sbi_write(addr_value => SPI_PRESCALER, data_value => x"0F", msg => "Set prescaler", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    --sbi_check(addr_value => SPI_PRESCALER, data_exp   => x"0F", msg => "Read back prescaler", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
 
-      cfg(clk_i,dut_ifi,dut_ifo,'0','0',X"0F");
+    log(ID_LOG_HDR, "Configure and verify SPI control registers", C_SCOPE);
+    sbi_write(addr_value => SPI_CFG      , data_value => x"01", msg => "Enable SPI", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_check(addr_value => SPI_CFG      , data_exp   => x"01", msg => "Read back enabled config", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
 
-      wait for 800 us;
-      
-      -- Read Instruction
-      cmd(clk_i,dut_ifi,dut_ifo,'1','0','0',X"3");
-      tx(clk_i,dut_ifi,dut_ifo,X"03");
-      -- Read Address
-      tx(clk_i,dut_ifi,dut_ifo,X"00");
-      tx(clk_i,dut_ifi,dut_ifo,X"00");
-      tx(clk_i,dut_ifi,dut_ifo,X"05");
-      -- Read Data
-      cmd(clk_i,dut_ifi,dut_ifo,'0','1','1',X"3");
---    tx(clk_i,dut_ifi,dut_ifo,X"00");
---    tx(clk_i,dut_ifi,dut_ifo,X"00");
---    tx(clk_i,dut_ifi,dut_ifo,X"00");
---    tx(clk_i,dut_ifi,dut_ifo,X"00");
+    wait for 800 us;
 
-    rx(clk_i,dut_ifi,dut_ifo,X"06");
-    rx(clk_i,dut_ifi,dut_ifo,X"07");
-    rx(clk_i,dut_ifi,dut_ifo,X"08");
-    rx(clk_i,dut_ifi,dut_ifo,X"09");
-    
-    report "[TESTBENCH] Test End";
-    test_ok   <= '1';
-    run(100);
-    test_done <= '1';
+
+    sbi_write(addr_value => SPI_CMD      , data_value => x"83", msg => "Write command register", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_write(addr_value => SPI_DATA     , data_value => x"03", msg => "SPI Instruction 0x03", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_write(addr_value => SPI_DATA     , data_value => x"00", msg => "SPI Address 0x000005", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_write(addr_value => SPI_DATA     , data_value => x"00", msg => "SPI Address 0x000005", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_write(addr_value => SPI_DATA     , data_value => x"05", msg => "SPI Address 0x000005", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+
+    sbi_write(addr_value => SPI_CMD      , data_value => x"63", msg => "Write command register", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_check(addr_value => SPI_DATA     , data_exp   => x"06", msg => "Read Byte @ 0x000005", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_check(addr_value => SPI_DATA     , data_exp   => x"07", msg => "Read Byte @ 0x000006", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_check(addr_value => SPI_DATA     , data_exp   => x"08", msg => "Read Byte @ 0x000007", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+    sbi_check(addr_value => SPI_DATA     , data_exp   => x"09", msg => "Read Byte @ 0x000008", clk => clk_i, sbi_if => sbi_if, config => C_SBI_BFM_CONFIG_SPI);
+
+--       cfg(clk_i,dut_ifi,dut_ifo,'0','0',X"0F");
+-- 
+--       wait for 800 us;
+--       
+--       -- Read Instruction
+--       cmd(clk_i,dut_ifi,dut_ifo,'1','0','0',X"3");
+--       tx(clk_i,dut_ifi,dut_ifo,X"03");
+--       -- Read Address
+--       tx(clk_i,dut_ifi,dut_ifo,X"00");
+--       tx(clk_i,dut_ifi,dut_ifo,X"00");
+--       tx(clk_i,dut_ifi,dut_ifo,X"05");
+--       -- Read Data
+--       cmd(clk_i,dut_ifi,dut_ifo,'0','1','1',X"3");
+-- --    tx(clk_i,dut_ifi,dut_ifo,X"00");
+-- --    tx(clk_i,dut_ifi,dut_ifo,X"00");
+-- --    tx(clk_i,dut_ifi,dut_ifo,X"00");
+-- --    tx(clk_i,dut_ifi,dut_ifo,X"00");
+-- 
+--     rx(clk_i,dut_ifi,dut_ifo,X"06");
+--     rx(clk_i,dut_ifi,dut_ifo,X"07");
+--     rx(clk_i,dut_ifi,dut_ifo,X"08");
+--     rx(clk_i,dut_ifi,dut_ifo,X"09");
+-- 
+--     
+    report "[TB_SPI] All SBI SPI checks passed";
+    report_alert_counters(FINAL);
+    std.env.stop;
     wait;
   end process;
-
-  
-    gen_test_done: process (test_done) is
-    begin  -- process gen_test_done
-      if test_done'event and test_done = '1' then  -- rising clock edge
-        if test_ok = '1' then
-          report "[TESTBENCH] Test OK";
-        else
-          report "[TESTBENCH] Test KO" severity failure;
-        end if;
-        
-      end if;
-    end process gen_test_done;
 
 end sim;
